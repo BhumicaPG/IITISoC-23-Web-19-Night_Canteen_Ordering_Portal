8bc1285d9d425b38e89b8e4fe218dc07d2f7d91f
mani-sahu <manisahu0912@gmail.com>
2023-07-07 22:18:57 +0530

    review form validation

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -5,17 +5,19 @@ import ReviewForm from './ReviewForm';
 const Review = () => {
     return (
         <motion.div className="w-full flex items-start justify-start flex-col px-12 ms:px-20">
-        <div className='w-full flex items-center justify-between'>
+            <div className='w-full flex items-center justify-between'>
                 <div className='flex flex-col items-start justify-start gap-1'>
-                        <p className='text-2xl text-headingColor font-bold'>
-                                Leave a reply
-                        </p>
-                        <div className='w-40 h-1 rounded-md bg-orange-500'></div>
+                    <p className='text-2xl text-headingColor font-bold'>
+                        Leave a review
+                    </p>
+                    <div className='w-40 h-1 rounded-md bg-orange-500'></div>
                 </div>
-        </div>
-        <div className="w-full pt-6 flex items-center justify-center gap-6 py-8">
-            <ReviewForm />
-        </div>
+            </div>
+
+            <div className="w-full pt-6 flex items-center justify-center gap-6 py-8">
+                <ReviewForm />
+            </div>
+
         </motion.div>
     );
 }
